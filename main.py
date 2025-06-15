5b7e9f89cd021473c0e1750a5aa71315bd1e3efd
VladimirLevikov <v.v.levikov@gmail.com>
2025-06-15 18:20:51 +0300

    Правки после ревью

--- main.py
+++ main.py
@@ -1,47 +1,33 @@
 import datetime
 import pandas
+import os
 from collections import defaultdict
+from dotenv import load_dotenv
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
-DATE_OF_FOUNDATION = 1920
+FOUNDATION_YEAR = 1920
 
 
-def create_product_dict() -> dict:
+def create_product_info() -> dict:
     """
     Function for read document and return dict with product information.
     :return: dict with product information
     """
 
-    excel_data_df = pandas.read_excel('wine3.xlsx', sheet_name='Лист1')
-    product_dict = defaultdict(list)
-    for data in excel_data_df.values:
-        category = data[0]
-        name = check_value(data[1])
-        kind = check_value(data[2])
-        cost = check_value(data[3])
-        img = check_value(data[4])
-        stock = check_value(data[5])
-
-        product_dict[category].append({'name': name,
+    excel_data_df = pandas.read_excel(os.getenv("PATH_TO_DOCUMENT"),
+                                      sheet_name=os.getenv("DOCUMENT_SHEET"),
+                                      na_values=['nan'],
+                                      keep_default_na=False)
+    product_info = defaultdict(list)
+    for category, name, kind, cost, img, stock in excel_data_df.values:
+        product_info[category].append({'name': name,
                                        'kind': kind,
                                        'cost': cost,
                                        'img': img,
                                        'stock': stock})
-    return product_dict
-
-
-def check_value(value):
-    """
-    Function check value and return empty string if value nan, or return value
-
-    :param value: - value for check
-    :return: return empty string or value
-    """
-    if str(value) == 'nan':
-        return ""
-    return value
+    return product_info
 
 
 def get_year_word(years: int) -> str:
@@ -65,23 +51,23 @@ def get_year_word(years: int) -> str:
         return "лет"
 
 
-env = Environment(
-    loader=FileSystemLoader('.'),
-    autoescape=select_autoescape(['html', 'xml'])
-)
+if __name__ == "__main__":
+    load_dotenv()
+    env = Environment(
+        loader=FileSystemLoader('.'),
+        autoescape=select_autoescape(['html', 'xml'])
+    )
 
-template = env.get_template('wine_template.html')
-company_age = datetime.datetime.now().year - DATE_OF_FOUNDATION
+    template = env.get_template('wine_template.html')
+    company_age = datetime.datetime.now().year - FOUNDATION_YEAR
 
-rendered_page = template.render(
-    company_age=f"{company_age} {get_year_word(company_age)}",
-    product_dict=create_product_dict()
-)
+    rendered_page = template.render(
+        company_age=f"{company_age} {get_year_word(company_age)}",
+        product_info=create_product_info()
+    )
 
-with open('index.html', 'w', encoding="utf8") as file:
-    file.write(rendered_page)
+    with open('index.html', 'w', encoding="utf8") as file:
+        file.write(rendered_page)
 
-
-server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
-server.serve_forever()
-
+    server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
+    server.serve_forever()
