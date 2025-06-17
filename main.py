aab848095698945877bcf4a5b5167c8ac067a755
VladimirLevikov <v.v.levikov@gmail.com>
2025-06-17 18:00:08 +0300

    Правки после ревью

--- main.py
+++ main.py
@@ -1,33 +1,30 @@
 import datetime
-import pandas
 import os
 from collections import defaultdict
-from dotenv import load_dotenv
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 
+import pandas
+from dotenv import load_dotenv
 from jinja2 import Environment, FileSystemLoader, select_autoescape
+from pandas import DataFrame
 
 FOUNDATION_YEAR = 1920
 
 
-def create_product_info() -> dict:
+def create_product_carts(excel_table: DataFrame) -> dict:
     """
     Function for read document and return dict with product information.
     :return: dict with product information
     """
 
-    excel_data_df = pandas.read_excel(os.getenv("PATH_TO_DOCUMENT"),
-                                      sheet_name=os.getenv("DOCUMENT_SHEET"),
-                                      na_values=['nan'],
-                                      keep_default_na=False)
-    product_info = defaultdict(list)
-    for category, name, kind, cost, img, stock in excel_data_df.values:
-        product_info[category].append({'name': name,
-                                       'kind': kind,
-                                       'cost': cost,
-                                       'img': img,
-                                       'stock': stock})
-    return product_info
+    product_carts = defaultdict(list)
+    for category, name, kind, cost, img, stock in excel_table.values:
+        product_carts[category].append({'name': name,
+                                        'kind': kind,
+                                        'cost': cost,
+                                        'img': img,
+                                        'stock': stock})
+    return product_carts
 
 
 def get_year_word(years: int) -> str:
@@ -53,6 +50,10 @@ def get_year_word(years: int) -> str:
 
 if __name__ == "__main__":
     load_dotenv()
+    excel_data_df = pandas.read_excel(os.getenv("PATH_TO_DOCUMENT"),
+                                      sheet_name=os.getenv("DOCUMENT_SHEET"),
+                                      na_values=['nan'],
+                                      keep_default_na=False)
     env = Environment(
         loader=FileSystemLoader('.'),
         autoescape=select_autoescape(['html', 'xml'])
@@ -63,7 +64,7 @@ if __name__ == "__main__":
 
     rendered_page = template.render(
         company_age=f"{company_age} {get_year_word(company_age)}",
-        product_info=create_product_info()
+        product_info=create_product_carts(excel_data_df)
     )
 
     with open('index.html', 'w', encoding="utf8") as file:
